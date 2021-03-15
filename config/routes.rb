f3f21a5d92ff99abf7c9e402d3403f293b9a64dd
minoru.tomioka <275practice@gmail.com>
2021-03-15 15:17:50 +0000

    tasklist for heroku.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: 'tasks#index'
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: 'tasks#index'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
