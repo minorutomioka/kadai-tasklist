f2269e7f3a0fbdac9306024e6c85aa6a27de219b
minoru.tomioka <275practice@gmail.com>
2021-03-15 14:00:32 +0000

    task list for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -12,7 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
     if @task.save
       flash[:success]='タスクが追加されました。'
       redirect_to @task
@@ -20,8 +25,6 @@ class TasksController < ApplicationController
       flash.now[:danger] ='タスクは追加できませんでした。'
       render :new
     end
-    
-    
   end
 
   def edit
@@ -53,5 +56,11 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
-  
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root to: 'tasks#index'
-  
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+ 
   resources :tasks
-  resources :sessions, only: [:new, :create, :destroy]
-  
+  resources :users, only: [:index, :show, :create]
 
 end
